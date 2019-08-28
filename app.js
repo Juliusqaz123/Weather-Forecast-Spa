03e513add019baf538cc125925bca323b0e59329
Julius <juliusblusevicius20@gmail.com>
2019-08-28 16:16:19 +0300

    START

---
+++ app.js
@@ -0,0 +1,23 @@
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.config(function ($routeProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl: 'pages/home.html',
+            controller: 'homeController'
+        })
+        .when('/forecast', {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastController'
+
+        })
+});
+
+weatherApp.controller('homeController', ['$scope', '$log', function($scope,$log){
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$log', function($scope,$log){
+
+}]);
